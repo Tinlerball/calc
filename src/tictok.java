3da1023f1decc12afb217f6da7010b12b8dfb514
mikhailovra <solider90@bk.ru>
2017-04-21 14:05:17 +0300

    v. 1.0

--- src/tictok.java
+++ src/tictok.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.regex.*;
 
 /**
  * Created by mikhailovra on 18.04.2017.
@@ -30,16 +31,55 @@ public class tictok {
         }
     }
 
+    public static String checkOp(char[] str){
+        String[] parse_str = new String[str.length];
+        int i = 0;
+        System.out.println(parse_str.length);
+        System.out.println(str.length);
+        while (str.length > i){
+            parse_str[i] = String.valueOf( str[i] );
+            i++;
+        }
+        String op = "";
+        for (int j = 0; parse_str.length > j; j++ ){
+            if (!parse_str[j].equals( "+" ) || !parse_str[j].equals( "-" ) || !parse_str[j].equals( "*" ) || !parse_str[j].equals( "/" ))
+                j++;
+            else
+                op = parse_str[j];
+        }
+        return op;
+    }
+
 
     public static void main(String[] args) throws IOException {
 
         InputStreamReader inputStreamReader = new InputStreamReader(System.in);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        String term1 = bufferedReader.readLine();
+        /*String term1 = bufferedReader.readLine();
         String op = bufferedReader.readLine();
         String term2 = bufferedReader.readLine();
 
         System.out.println(calc( term1, op, term2 ));
+        */
+        String str = bufferedReader.readLine();
+        char[] cs = str.toCharArray();
+
+        String[] str_array = str.split( "\\W" );
+        System.out.println(calc( str_array[0],checkOp( cs ), str_array[1] ));
+
+
+
+        /*if(str.matches("^[0-9]*\\s*[\\D]\\s*[0-9]*$")) {
+            int i = 0;
+            while (cs.equals( "\\d" ){
+
+
+            }
+        }*/
+        //System.out.println(calc(split[0], split[1],split[2]));
+
+
+
     }
 
 
